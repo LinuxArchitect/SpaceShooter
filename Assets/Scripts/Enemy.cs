f50a4a1d9d77e9b12297b00b4ad55b0a3a51bb01
George Boyce <george.r.boyce@gmail.com>
2024-01-18 17:11:02 -0500

    c003, add Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Enemy : MonoBehaviour
+{
+    // FIXME, make these configurable
+    [SerializeField] private float _speed = 4.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // get our screen position in pixels
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        Vector3 bottomLeftOfScreen = new Vector3(0f, 0f, 0f);
+        Vector3 leftBottomOfScreen = Camera.main.ScreenToWorldPoint(bottomLeftOfScreen);
+        Vector3 rightTopOfScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        
+        transform.Translate(Vector3.down * (_speed * Time.deltaTime));
+
+        if (screenPos.y <= 0) // wrap moving down off bottom edge
+        {
+            float randomX = Random.Range(leftBottomOfScreen.x, rightTopOfScreen.x);
+            //Debug.Log($"ENEMY: wrap down, screen pos y: {screenPos.y} bottom: 0, to {randomX}, {rightTopOfScreen.y}");
+            transform.position = new Vector3(randomX, rightTopOfScreen.y, 0f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Enemy hit Player");
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+            Destroy(this.gameObject);
+            
+        } else if (other.CompareTag("Laser"))
+        {
+            Debug.Log("Laser hit Enemy");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        } else
+        {
+            Debug.Log($"Enemy hit by {other.transform.name}");
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,14 +8,17 @@ public class Player : MonoBehaviour
     // FIXME, make these configurable
     [SerializeField] private float _speed = 10.0f;  
     [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private float _laserSpawnOffset = 0.8f; 
     [SerializeField] private float fireRate = 0.2f;
+    [SerializeField] private int _lives = 3;
 
     private float nextFireTime = 0f;
+    private float nextSpawnTime = 3f;
     
     void Start()
     {
-        Debug.Log("Hello: " + gameObject.name);
+        //Debug.Log("Hello: " + gameObject.name);
 
         // set starting location
         transform.position = new Vector3(0f, 0f, 0f);
@@ -24,6 +28,18 @@ public class Player : MonoBehaviour
     {
         ProcessWASD();
         ProcessSpaceKey();
+        
+        // temp spawn system for enemy
+        if (Time.time > nextSpawnTime)
+        {
+            Vector3 bottomLeftOfScreen = new Vector3(0f, 0f, 0f);
+            Vector3 leftBottomOfScreen = Camera.main.ScreenToWorldPoint(bottomLeftOfScreen);
+            Vector3 rightTopOfScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+            float randomX = Random.Range(leftBottomOfScreen.x, rightTopOfScreen.x);
+            var enemySpawnPosition = new Vector3(randomX, rightTopOfScreen.y, 0f);
+            Instantiate(_enemyPrefab, enemySpawnPosition, Quaternion.identity);
+            nextSpawnTime = Time.time + Random.Range(0.5f, 3f);
+        }
     }
 
     void ProcessWASD()
@@ -48,22 +64,22 @@ public class Player : MonoBehaviour
         // test all four screen edge boundaries, and reset world position as necessary
         if (screenPos.x <= 0 && direction.x < 0) // wrap moving left off left edge
         {
-            Debug.Log($"wrap left, screen pos x: {screenPos.x}, left edge: 0");
+            //Debug.Log($"wrap left, screen pos x: {screenPos.x}, left edge: 0");
             transform.position = new Vector3(rightTopOfScreen.x, transform.position.y, 0f);
             
         } else if (screenPos.x >= Screen.width && direction.x > 0) // wrap moving right off right edge
         {
-            Debug.Log($"wrap right, screen pos x: {screenPos.x} right edge: {Screen.width}");
+            //Debug.Log($"wrap right, screen pos x: {screenPos.x} right edge: {Screen.width}");
             transform.position = new Vector3(leftBottomOfScreen.x, transform.position.y, 0f);
             
         } else if (screenPos.y <= 0 && direction.y < 0) // limit moving down off bottom edge
         {
-            Debug.Log($"limit down, screen pos y: {screenPos.y} bottom: 0");
+            //Debug.Log($"limit down, screen pos y: {screenPos.y} bottom: 0, direction: {direction}");
             transform.position = new Vector3(transform.position.x, leftBottomOfScreen.y, 0f);
             
         } else if (screenPos.y > screenLimitY && direction.y > 0) // limit moving up to midpoint of screen
         {
-            Debug.Log($"limit up, screen pos y: {screenPos.y} midpoint: {screenLimitY}");
+            //Debug.Log($"limit up, screen pos y: {screenPos.y} midpoint: {screenLimitY}");
             transform.position = new Vector3(transform.position.x, worldLimitY, 0f);
         }
     }
@@ -76,6 +92,26 @@ public class Player : MonoBehaviour
             var laserSpawnPosition = transform.position + new Vector3(0f, _laserSpawnOffset, 0f);
             Instantiate(_laserPrefab, laserSpawnPosition, Quaternion.identity);
             nextFireTime = Time.time + fireRate;
+        }
+        // REMOVEME
+        if (Input.GetKeyDown(KeyCode.Period))
+        {
+            Vector3 bottomLeftOfScreen = new Vector3(0f, 0f, 0f);
+            Vector3 leftBottomOfScreen = Camera.main.ScreenToWorldPoint(bottomLeftOfScreen);
+            Vector3 rightTopOfScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+            float randomX = Random.Range(leftBottomOfScreen.x, rightTopOfScreen.x);
+            var enemySpawnPosition = new Vector3(randomX, rightTopOfScreen.y, 0f);
+            Instantiate(_enemyPrefab, enemySpawnPosition, Quaternion.identity);
+        }
+    }
+
+    public void Damage()
+    {
+        _lives--;
+        if (_lives < 1)
+        {
+            Debug.Log("Game Over");
+            Destroy(this.gameObject);
         }
     }
 }
